371efa145e1278ce0377f5a39409e011005a7ec7
nagsai-nani <133091211+nagsai-nani@users.noreply.github.com>
2023-05-10 16:05:13 +0530

    Initial commit

---
+++ src/main/java/com/example/urdc/UrdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.urdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrdcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/urdc/models/User.java
@@ -0,0 +1,5 @@
+package com.example.urdc.models;
+
+public class User {
+
+}
